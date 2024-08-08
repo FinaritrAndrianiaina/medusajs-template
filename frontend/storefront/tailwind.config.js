50ce0332eee276e4cf786e9242512b0e59c86185
andrianiaina <finaritrandrianiaina@gmail.com>
2024-08-08 09:54:37 +0300

    chore(repository): restore lib

--- frontend/storefront/tailwind.config.js
+++ frontend/storefront/tailwind.config.js
@@ -2,7 +2,7 @@ const path = require("path")
 
 module.exports = {
   darkMode: "class",
-  presets: [require("@medusajs/ui-preset")],
+  presets: [require("@template/ui-preset")],
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx}",
